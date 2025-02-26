5d7cfca0baa1b6f2a38b69087348a6817c74181d
Valentina Ovsianik <forovp@gmail.com>
2025-02-26 21:10:05 +0300

    Описаны представления и маршруты для модели пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    """Получение списка всех пользователей"""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    """Создание нового пользователя"""
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password) # Хэширование пароля
+        user.save()
