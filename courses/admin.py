c4d1f6952da6eaa4b83001b4537cd17008097e80
Valentina Ovsianik <forovp@gmail.com>
2025-02-26 22:11:47 +0300

    Модели курса и урока зарегистрированы в админ

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Course, Lesson
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "owner")
+    search_fields = ("name", "owner__email") # Поиск по названию курса и email владельца
+    list_filter = ("owner",) # Фильтр по владельцу курса
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "course", "owner")
+    search_fields = ("title", "course__name", "owner__email") # Поиск по названию урока и курса и email владельца
+    list_filter = ("course",) # Фильтр по курсу
