5941b1e5beb036cd99464060e8db800c60327b49
Valentina Ovsianik <forovp@gmail.com>
2025-02-27 00:08:13 +0300

    Обновлены прва пользователей с учетом добавления моделей

--- users/management/commands/create_groups.py
+++ users/management/commands/create_groups.py
@@ -1,7 +1,9 @@
-from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
-from courses.models import Course, Lesson
+from django.core.management.base import BaseCommand
+
+from courses.models import Answer, Course, Lesson, Question, Test, TestResult
+
 
 class Command(BaseCommand):
     "Создание групп пользователей и назначение им прав"
@@ -12,26 +14,56 @@ class Command(BaseCommand):
         teacher_group, _ = Group.objects.get_or_create(name="Преподаватели")
         student_group, _ = Group.objects.get_or_create(name="Студенты")
 
-        # Получаем разрешения для моделей Course и Lesson
+        # Получаем разрешения для моделей
         course_ct = ContentType.objects.get_for_model(Course)
         lesson_ct = ContentType.objects.get_for_model(Lesson)
-
+        test_ct = ContentType.objects.get_for_model(Test)
+        question_ct = ContentType.objects.get_for_model(Question)
+        answer_ct = ContentType.objects.get_for_model(Answer)
+        test_result_ct = ContentType.objects.get_for_model(TestResult)
 
         # Администраторам предоставлен полный доступ
         admin_group.permissions.set(Permission.objects.all())
 
-        # Преподаватели могут создавать, редактировать, просматривать и удалять свои курсы, уроки
+        # Преподаватели могут выполнять все действия со своими курсами, уроками, тестами
         teacher_permissions = Permission.objects.filter(
-            content_type__in=[course_ct, lesson_ct],
-            codename__in=["add_course", "view_course", "change_course", "delete_course",
-                          "add_lesson", "view_lesson", "change_lesson", "delete_lesson",
-                          ]
+            content_type__in=[course_ct, lesson_ct, test_ct, question_ct, answer_ct],
+            codename__in=[
+                "add_course",
+                "view_course",
+                "change_course",
+                "delete_course",
+                "add_lesson",
+                "view_lesson",
+                "change_lesson",
+                "delete_lesson",
+                "add_test",
+                "view_test",
+                "change_test",
+                "delete_test",
+                "add_question",
+                "view_question",
+                "change_question",
+                "delete_question",
+                "add_answer",
+                "view_answer",
+                "change_answer",
+                "delete_answer",
+            ],
         )
         teacher_group.permissions.set(teacher_permissions)
 
-        # Студенты могут только просматривать
+        # Студенты могут только просматривать курсы, уроки, тесты и вопросы, выполнять тесты
         student_permissions = Permission.objects.filter(
-            content_type__in=[course_ct, lesson_ct], codename__in=["view_course", "view_lesson"]
+            content_type__in=[course_ct, lesson_ct, test_ct, question_ct, test_result_ct],
+            codename__in=[
+                "view_course",
+                "view_lesson",
+                "view_test",
+                "view_question",
+                "add_testresult",
+                "view_testresult",
+            ],
         )
         student_group.permissions.set(student_permissions)
 
--- users/permissions.py
+++ users/permissions.py
@@ -1,16 +1,22 @@
 from rest_framework import permissions
 
+
 class IsAdmin(permissions.BasePermission):
     """Доступ для администраторов"""
+
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.groups.filter(name="Администраторы").exists()
 
+
 class IsTeacher(permissions.BasePermission):
     """Доступ для преподавателей"""
+
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.groups.filter(name="Преподаватели").exists()
 
+
 class IsStudent(permissions.BasePermission):
     """Доступ для студентов"""
+
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.groups.filter(name="Студенты").exists()
