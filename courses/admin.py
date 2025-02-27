67fd2904896b775dc45942cefa1075c4410484dd
Valentina Ovsianik <forovp@gmail.com>
2025-02-27 17:57:38 +0300

    Регистрация моделей в админ

--- courses/admin.py
+++ courses/admin.py
@@ -1,16 +1,45 @@
 from django.contrib import admin
-from .models import Course, Lesson
+
+from .models import Answer, Course, Lesson, Question, Test, TestResult
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "owner")
-    search_fields = ("name", "owner__email") # Поиск по названию курса и email владельца
-    list_filter = ("owner",) # Фильтр по владельцу курса
+    search_fields = ("name", "owner__email")  # Поиск по названию курса и email владельца
+    list_filter = ("owner",)  # Фильтр по владельцу курса
 
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "course", "owner")
-    search_fields = ("title", "course__name", "owner__email") # Поиск по названию урока и курса и email владельца
-    list_filter = ("course",) # Фильтр по курсу
+    search_fields = ("title", "course__name", "owner__email")  # Поиск по названию урока и курса и email владельца
+    list_filter = ("course",)  # Фильтр по курсу
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ("title", "lesson", "owner")
+    list_filter = ("lesson", "owner")
+    search_fields = ("title", "owner__email")
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("text", "test")
+    list_filter = ("test",)
+    search_fields = ("text",)
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("text", "question", "is_correct")
+    list_filter = ("question", "is_correct")
+    search_fields = ("text",)
+
+
+@admin.register(TestResult)
+class TestResultAdmin(admin.ModelAdmin):
+    list_display = ("test", "student", "score", "completed_at")
+    list_filter = ("test", "student")
+    search_fields = ("student__email", "test__title")
