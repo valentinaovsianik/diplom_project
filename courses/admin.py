2d08cdf3b1e328ce263ce12b8f38ed21c9c73ee4
Valentina Ovsianik <forovp@gmail.com>
2025-03-03 15:45:04 +0300

    Добавлены inline-модели для вопросов и ответов

--- courses/admin.py
+++ courses/admin.py
@@ -3,6 +3,24 @@ from django.contrib import admin
 from .models import Answer, Course, Lesson, Question, Test, TestResult
 
 
+class AnswerInline(admin.TabularInline):
+    """Ответы видны в админке внутри вопросов"""
+
+    model = Answer
+    extra = 3  # 3 пустых поля для новых ответов
+    min_num = 1  # Минимум 1 ответ
+    show_change_link = True  # Добавляем кнопку редактирования
+
+
+class QuestionInline(admin.TabularInline):
+    """Вопросы с ответами видны в админке внутри тестов"""
+
+    model = Question
+    extra = 2  # Количество пустых полей для вопросов
+    show_change_link = True  # Кнопка редактирования вопроса
+    inlines = [AnswerInline]  # Показываем ответы внутри вопросов
+
+
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "owner")
@@ -22,6 +40,7 @@ class TestAdmin(admin.ModelAdmin):
     list_display = ("title", "lesson", "owner")
     list_filter = ("lesson", "owner")
     search_fields = ("title", "owner__email")
+    inlines = [QuestionInline]  # Показываем вопросы внутри теста
 
 
 @admin.register(Question)
@@ -29,6 +48,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ("text", "test")
     list_filter = ("test",)
     search_fields = ("text",)
+    inlines = [AnswerInline]  # Показывает ответы внутри вопроса
 
 
 @admin.register(Answer)
